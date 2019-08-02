d6e6ce7ba827c21ac1ac281bd4aead1944740e32
WolfOfProgramming <47081011+WolfOfProgramming@users.noreply.github.com>
2019-08-02 20:07:00 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const el = document.getElementById("sumbit");
-el.addEventListener("click", addThing, false);
+el.addEventListener("sumbit", addThing, false);
 
 function addThing() {
-    
+    document.preventDefault();
     const elValue = document.getElementById("AddThingToDo").value;
     const li = document.createElement("li");
     li.textContent = elValue;
